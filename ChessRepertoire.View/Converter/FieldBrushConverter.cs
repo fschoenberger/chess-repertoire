65e645568ac6e82c2a8ff0adddf284398ca97a8b
Frederic Schönberger <24734883+fschoenberger@users.noreply.github.com>
2023-05-21 00:53:42 +0200

    Moved board's color to theme.

--- ChessRepertoire.View/Converter/FieldBrushConverter.cs
+++ ChessRepertoire.View/Converter/FieldBrushConverter.cs
@@ -1,37 +1,28 @@
 ﻿using System;
 using System.Globalization;
+using System.Windows;
 using System.Windows.Data;
 using System.Windows.Media;
 
 namespace ChessRepertoire.View.Wpf.Converter {
     public class FieldBrushConverter : IMultiValueConverter {
-        private readonly Brush _whiteBrush;
+        private readonly Brush _whiteBrush = Application.Current.FindResource("LightSquareBrush") as Brush;
 
-        private readonly Brush _blackBrush;
+        private readonly Brush _blackBrush = Application.Current.FindResource("DarkSquareBrush") as Brush;
 
-        public FieldBrushConverter()
-        {
-            _whiteBrush = new SolidColorBrush(Colors.NavajoWhite);
-            _blackBrush = new SolidColorBrush(Colors.Peru);
-        }
-
-        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
-        {
-            if (values.Length != 2 || !typeof(Brush).IsAssignableFrom(targetType))
-            {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture) {
+            if (values.Length != 2 || !typeof(Brush).IsAssignableFrom(targetType)) {
                 throw new ArgumentException("Invalid arguments");
             }
 
-            if (values[0] is int row && values[1] is int column)
-            {
+            if (values[0] is int row && values[1] is int column) {
                 return (row + column) % 2 == 0 ? _whiteBrush : _blackBrush;
             }
 
             return null;
         }
 
-        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
-        {
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) {
             return null;
         }
     }
