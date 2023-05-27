1c5bd99feb8f970fbc06d85b1d890b43aeb18475
Frederic Schönberger <24734883+fschoenberger@users.noreply.github.com>
2023-05-27 22:48:12 +0200

    Added eval bar stub.

---
+++ ChessRepertoire.ViewModel/EvaluationBar/IEvaluationBarViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReactiveUI;
+
+namespace ChessRepertoire.ViewModel.EvaluationBar {
+    public interface IEvaluationBarViewModel : IReactiveObject {
+        string TopText { get; }
+
+        string BottomText { get; }
+
+        double Percentage { get; }
+    }
+
+    public class DesignTimeEvaluationBarViewModel : ReactiveObject, IEvaluationBarViewModel {
+        public string TopText => "10.3";
+        public string BottomText => "";
+        public double Percentage => 0.9;
+    }
+}
