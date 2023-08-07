18ff68cafa1dbd09ee2e8627b4780c6dba049195
Frederic Schönberger <24734883+fschoenberger@users.noreply.github.com>
2023-08-07 14:14:10 +0200

    Added on click handlers for fields and pieces.

--- ChessRepertoire.View/Control/Board/ChessBoard.xaml.cs
+++ ChessRepertoire.View/Control/Board/ChessBoard.xaml.cs
@@ -1,20 +1,24 @@
-﻿using System.Reactive.Disposables;
+﻿using System;
+using System.Diagnostics;
+using System.Reactive.Disposables;
 using ChessRepertoire.ViewModel.Board;
 using ReactiveMarbles.ObservableEvents;
 using System.Windows.Controls;
+using System.Windows.Shapes;
 using ReactiveUI;
 
-namespace ChessRepertoire.View.Wpf.Control.Board {
-    /// <summary>
-    /// Interaction logic for ChessBoard.xaml
-    /// </summary>
-    public partial class ChessBoard : ReactiveUserControl<IBoardViewModel> {
-        public ChessBoard() {
+namespace ChessRepertoire.View.Wpf.Control.Board
+{
+    public partial class ChessBoard : ReactiveUserControl<IBoardViewModel>
+    {
+        public ChessBoard()
+        {
             InitializeComponent();
 
             ViewModel = new BoardViewModel();
 
-            this.WhenActivated(d => {
+            this.WhenActivated(d =>
+            {
                 this.OneWayBind(ViewModel,
                         viewModel => viewModel.Fields,
                         view => view.Fields.ItemsSource)
@@ -26,6 +30,20 @@ namespace ChessRepertoire.View.Wpf.Control.Board {
                     .DisposeWith(d);
 
                 this.BindCommand(ViewModel, vm => vm.FlipBoard, v => v.FlipButton).DisposeWith(d);
+
+                Fields.Events().MouseDown.Subscribe((e) => {
+                    if (e.OriginalSource is Border { DataContext: IFieldViewModel field })
+                    {
+                        Debug.WriteLine($"Click on {(char)('A' + field.File)}{field.Rank + 1}");
+                    }
+                }).DisposeWith(d);
+
+                Pieces.Events().MouseDown.Subscribe((e) => {
+                    if (e.OriginalSource is Path { DataContext: IPieceViewModel piece })
+                    {
+                        Debug.WriteLine($"Click on {piece.Color} {piece.Type} at {(char)('A' + piece.File)}{piece.Rank + 1}");
+                    }
+                }).DisposeWith(d);
             });
         }
     }
