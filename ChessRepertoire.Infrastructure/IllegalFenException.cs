07b84d6847674d5998b4510a364e0d8b8b44da19
Frederic Schönberger <24734883+fschoenberger@users.noreply.github.com>
2023-08-12 00:01:45 +0200

    Moved exception to infrastructure layer

---
+++ ChessRepertoire.Infrastructure/IllegalFenException.cs
@@ -0,0 +1,10 @@
+﻿namespace ChessRepertoire.Infrastructure;
+
+public class IllegalFenException : Exception
+{
+    public IllegalFenException(string message) : base(message)
+    {
+    }
+
+    public IllegalFenException(string message, Exception innerException) : base(message, innerException) { }
+}
