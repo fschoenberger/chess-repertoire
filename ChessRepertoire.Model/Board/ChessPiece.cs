8aee0bfc9bf1fb550ac69846fd258f8cd157fc90
Frederic Schönberger <24734883+fschoenberger@users.noreply.github.com>
2023-08-26 15:05:21 +0200

    Added way to calculate posible moves (i.e. moves that are not out of bounds) to the pieces

--- ChessRepertoire.Model/Board/ChessPiece.cs
+++ ChessRepertoire.Model/Board/ChessPiece.cs
@@ -69,6 +69,106 @@ public record ChessPiece(Color Color, PieceType Type, Square Square)
         return false;
     }
 
+    private IEnumerable<Move> GetPotentialKnightMoves()
+    {
+        int[] knightMovesX = { 2, 1, -1, -2, -2, -1, 1, 2 };
+        int[] knightMovesY = { 1, 2, 2, 1, -1, -2, -2, -1 };
+
+        for (var i = 0; i < knightMovesX.Length; i++)
+        {
+            var move = new Move(Square, new Square(Square.File + knightMovesX[i], Square.Rank + knightMovesY[i]));
+            if (move.IsPotentiallyValid())
+                yield return move;
+        }
+    }
+
+    private IEnumerable<Move> GetPotentialPawnMoves()
+    {
+        var forwardDirection = Color == Color.White ? 1 : -1;
+        var startingRank = Color == Color.White ? 1 : 6; // Zero-based indexing
+
+        // One step forward
+        var forwardMove = new Move(Square, Square with { Rank = Square.Rank + forwardDirection });
+        if (forwardMove.IsPotentiallyValid())
+            yield return forwardMove;
+
+        // Two steps forward if on starting rank
+        if (Square.Rank == startingRank)
+        {
+            var doubleForwardMove = new Move(Square, Square with { Rank = Square.Rank + 2 * forwardDirection });
+            if (doubleForwardMove.IsPotentiallyValid())
+                yield return doubleForwardMove;
+        }
+
+        // Diagonal captures
+        var diagonalRight = new Move(Square, new Square(Square.File + 1, Square.Rank + forwardDirection));
+        if (diagonalRight.IsPotentiallyValid())
+            yield return diagonalRight;
+
+        var diagonalLeft = new Move(Square, new Square(Square.File - 1, Square.Rank + forwardDirection));
+        if (diagonalLeft.IsPotentiallyValid())
+            yield return diagonalLeft;
+    }
+
+    private IEnumerable<Move> GetPotentialRookMoves()
+    {
+        for (var i = 0; i < 8; ++i)
+        {
+            if (i != Square.File) yield return new Move(Square, Square with { File = i });
+            if (i != Square.Rank) yield return new Move(Square, Square with { Rank = i });
+        }
+    }
+
+    private IEnumerable<Move> GetPotentialBishopMoves()
+    {
+        // Diagonal moves
+        for (var i = 1; i <= 7; i++)
+        {
+            var upRight = new Move(Square,new Square(Square.File + i, Square.Rank + i));
+            if (upRight.IsPotentiallyValid())
+                yield return upRight; // Diagonal up-right
+
+            var downLeft = new Move(Square,new Square(Square.File - i, Square.Rank - i));
+            if (downLeft.IsPotentiallyValid())
+                yield return downLeft; // Diagonal down-left
+
+            var downRight = new Move(Square,new Square(Square.File + i, Square.Rank - i));
+            if (downRight.IsPotentiallyValid())
+                yield return downRight; // Diagonal down-right
+
+            var upLeft = new Move(Square,new Square(Square.File - i, Square.Rank + i));
+            if (upLeft.IsPotentiallyValid())
+                yield return upLeft; // Diagonal up-left
+        }
+    }
+
+    private IEnumerable<Move> GetPotentialKingMoves()
+    {
+        int[] kingMovesX = { 0, 1, 1, 1, 0, -1, -1, -1 };
+        int[] kingMovesY = { 1, 1, 0, -1, -1, -1, 0, 1 };
+
+        for (var i = 0; i < kingMovesX.Length; i++)
+        {
+            var move = new Move(Square,new Square(Square.File + kingMovesX[i], Square.Rank + kingMovesY[i]));
+            if (move.IsPotentiallyValid())
+                yield return move;
+        }
+    }
+
+    public IEnumerable<Move> GetPotentiallyLegalMoves()
+    {
+        return Type switch
+        {
+            PieceType.Pawn => GetPotentialPawnMoves(),
+            PieceType.Knight => GetPotentialKnightMoves(),
+            PieceType.Bishop => GetPotentialBishopMoves(),
+            PieceType.Rook => GetPotentialRookMoves(),
+            PieceType.King => GetPotentialKingMoves(),
+            PieceType.Queen => GetPotentialRookMoves().Concat(GetPotentialBishopMoves()),
+            _ => Enumerable.Empty<Move>(),
+        };
+    }
+
     public override string ToString()
     {
         return $"{Color} {Type} at {Square}";
