c841ac31d7aad43fe93635698e2917a987c771d8
Frederic Schönberger <24734883+fschoenberger@users.noreply.github.com>
2023-09-04 14:37:33 +0200

    Added dummy move explorer view model

---
+++ ChessRepertoire.ViewModel/MoveExplorer/MoveExplorerViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChessRepertoire.Model.Board;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+namespace ChessRepertoire.ViewModel.MoveExplorer
+{
+    public class MoveExplorerViewModel : ReactiveObject {
+        private readonly ObservableCollection<MoveViewModel> _moves;
+        public ObservableCollection<MoveViewModel> Moves => _moves;
+
+        public MoveExplorerViewModel(ChessBoard board)
+        {
+            _moves = new ObservableCollection<MoveViewModel>{
+                new() { FullMoveNumber = 1 },
+                new() { FullMoveNumber = 2 },
+                new() { FullMoveNumber = 3},
+                new() { FullMoveNumber = 4, Children = new List<MoveViewModel>{
+                    new() { FullMoveNumber = 5 },
+                    new() { FullMoveNumber = 6 },
+                    new() { FullMoveNumber = 7 },
+                }}
+            };
+
+            Debug.WriteLine("Done.");
+        }
+    }
+}
---
+++ ChessRepertoire.ViewModel/MoveExplorer/MoveViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReactiveUI;
+
+namespace ChessRepertoire.ViewModel.MoveExplorer
+{
+    public class MoveViewModel : ReactiveObject
+    {
+        public int FullMoveNumber { get; set; }
+
+        public string WhiteMoveSan { get; set; }
+
+        public string BlackMoveSan { get; set; }
+
+        public string WhiteMoveComment { get; set; }
+
+        public string BlackMoveComment { get; set; }
+
+        public IEnumerable<MoveViewModel> Children { get; set; }
+    }
+}
