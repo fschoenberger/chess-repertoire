142881c1ab5677b153f1b87e5474c64b11d5474f
Frederic Schönberger <24734883+fschoenberger@users.noreply.github.com>
2023-09-04 14:37:19 +0200

    Added dummy move explorer.

---
+++ ChessRepertoire.View/Control/MoveExplorer/MoveExplorerControl.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ChessRepertoire.ViewModel.MoveExplorer;
+using ReactiveUI;
+
+namespace ChessRepertoire.View.Wpf.Control.MoveExplorer
+{
+    public partial class MoveExplorerControl : ReactiveUserControl<MoveExplorerViewModel>
+    {
+        public MoveExplorerControl()
+        {
+            InitializeComponent();
+
+            this.WhenActivated(d => {
+                this.OneWayBind(ViewModel,
+                        viewModel => viewModel.Moves,
+                        view => view.MoveTree.ItemsSource)
+                    .DisposeWith(d);
+            });
+        }
+    }
+}
